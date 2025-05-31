08630eba0aa9fa8db0bb9f2f1c8be1fd6bb59343
nelioasousa <corneliosousaphb@gmail.com>
2025-05-30 22:45:02 -0300

    cana como um package puro e não namespace

---
+++ src/cana/__init__.py
@@ -0,0 +1,6 @@
+from cana import fib
+from cana import inversion
+from cana import majority
+from cana import msort
+
+__all__ = [fib.__name__, inversion.__name__, majority.__name__, msort.__name__]
