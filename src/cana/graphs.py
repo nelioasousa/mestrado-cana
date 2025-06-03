119d5b8a48f18865d5903adf9ae348c056d4e90a
nelioasousa <corneliosousaphb@gmail.com>
2025-06-02 23:46:54 -0300

    Módulo inicial para grafos

---
+++ src/cana/graphs.py
@@ -0,0 +1,95 @@
+"""Graphs representation using Python."""
+
+from collections.abc import Hashable, Collection, Callable
+import yaml
+
+
+type Edge = tuple[Vertex, Vertex, tuple]
+type KeyParser = Callable[[str], Hashable]
+type ValueParser = Callable[[str], object]
+
+
+class Vertex:
+    def __init__(self, key: Hashable, value: object):
+        self.key = key
+        self.value = value
+
+    def __hash__(self):
+        return hash(self.key)
+
+    def __eq__(self, value):
+        if isinstance(value, type(self)):
+            return self.key == value.key
+        else:
+            return False
+
+
+class WeightedUndirectedGraph:
+    """Weighted, undirected graph using adjacency list (hash map)."""
+
+    def __init__(self, vertices: Collection[Vertex], edges: Collection[Edge]):
+        self.edges = list(edges)
+        # dict comprehension needed so each list() is unique for each vertice
+        self.adjacency = {v: list() for v in vertices}
+        for lft, rgt, weigth in edges:
+            self.adjacency.setdefault(lft, list()).append((rgt, weigth))
+            self.adjacency.setdefault(rgt, list()).append((lft, weigth))
+
+    @classmethod
+    def from_file(
+        cls,
+        file_path: str,
+        key_parser: KeyParser = str,
+        value_parser: ValueParser = str,
+    ):
+        """Load a graph from a file."""
+        with open(file_path, mode="r") as f:
+            gf = yaml.load(f, Loader=yaml.BaseLoader)
+        vertices = {
+            k: Vertex(key_parser(k), value_parser(v))
+            for k, v in gf["vertices"].items()
+        }
+        edges = []
+        for edge in gf["edges"]:
+            lft, rgt, weight = edge.strip().split("::")
+            edges.append((vertices[lft], vertices[rgt], float(weight)))
+        return cls(list(vertices.values()), edges)
+
+    def insert_edge(self, edge: Edge) -> None:
+        """Insert an edge into the graph."""
+        self.adjacency.setdefault(edge[0], list()).append((edge[1], edge[2]))
+        self.adjacency.setdefault(edge[1], list()).append((edge[0], edge[2]))
+        self.edges.append(edge)
+        return None
+
+    def insert_edges(self, edges: Collection[Edge]) -> None:
+        """Insert many edges into the graph."""
+        for lft, rgt, weigth in edges:
+            self.adjacency.setdefault(lft, list()).append((rgt, weigth))
+            self.adjacency.setdefault(rgt, list()).append((lft, weigth))
+        self.edges.extend(edges)
+
+    def insert_vertex(self, vertex: Vertex) -> None:
+        """Insert a vertex into the graph."""
+        self.adjacency.setdefault(vertex, list())
+        return None
+
+    def insert_vertices(self, vertices: Collection[Vertex]) -> None:
+        """Insert many vertices into the graph."""
+        self.adjacency.update(
+            {v: list() for v in vertices if v not in self.adjacency}
+        )
+
+    def get_vertices(self) -> list[Vertex]:
+        """Return all the vertices in the graph."""
+        return list(self.adjacency)
+
+    def get_edges(self) -> list[Edge]:
+        """Return all the edges in the graph."""
+        return self.edges
+
+    def check_connection(self, a: Vertex, b: Vertex, /) -> bool:
+        """Check if two vertices are connected."""
+        if a not in self.adjacency:
+            return False
+        return b in tuple(i[0] for i in self.adjacency[a])
